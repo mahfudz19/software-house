a595500f71b68dd89db6810e08a3e4c7c3d66bfd
Mahfudz Masyhur <mfz.masyhur97@gmail.com>
2026-06-14 00:04:53 +0700

    feat: add turbopack configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  turbopack: {
+    root: "/Users/m/Documents/Github/software-house",
+  },
 };
 
 export default nextConfig;
