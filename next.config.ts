b91e2598b288b06d5e53dc614dfe9977d1447320
Mahfudz Masyhur <mfz.masyhur97@gmail.com>
2026-06-14 00:37:11 +0700

    feat: remove turbopack configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  turbopack: {
-    root: "/Users/m/Documents/Github/software-house",
-  },
 };
 
 export default nextConfig;
